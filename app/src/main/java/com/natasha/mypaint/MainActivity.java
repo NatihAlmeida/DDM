f6912f0f6baa04767ab8990cbf8c69eb46e015c1
Natasha <natashamariadealmeida@gmail.com>
2026-06-22 23:30:56 -0300

    Paint com diferentes formas ganhou de 10 a zero pra mim

--- app/src/main/java/com/natasha/mypaint/MainActivity.java
+++ app/src/main/java/com/natasha/mypaint/MainActivity.java
@@ -1,16 +1,11 @@
 package com.natasha.mypaint;
-
 import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.skydoves.colorpickerview.ColorEnvelope;
 import com.skydoves.colorpickerview.ColorPickerDialog;
@@ -19,18 +14,36 @@ import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
 public class MainActivity extends AppCompatActivity {
     SimplePaint simplePaint;
     ImageView imColorPicker;
+    Button linha;
+    Button circulo;
+    Button quadrado;
+    Button limpar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         simplePaint = findViewById(R.id.simplePaint3);
+        linha = findViewById(R.id.linha);
+        circulo = findViewById(R.id.circulo);
+        quadrado = findViewById(R.id.quadrado);
+        limpar = findViewById(R.id.limpar);
         imColorPicker=findViewById(R.id.imColorPicker);
+
         imColorPicker.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 colorPickerSelectColor();
+
             }
+        });
+
+        linha.setOnClickListener(v -> simplePaint.botaoSelecionado = 1);
+        circulo.setOnClickListener(v -> simplePaint.botaoSelecionado = 2);
+        quadrado.setOnClickListener(v -> simplePaint.botaoSelecionado = 3);
+        limpar.setOnClickListener(v -> {
+            simplePaint.limparPaint();
         });
     }
     public void colorPickerSelectColor(){
--- app/src/main/java/com/natasha/mypaint/SimplePaint.java
+++ app/src/main/java/com/natasha/mypaint/SimplePaint.java
@@ -17,20 +17,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SimplePaint extends View {
+    public int botaoSelecionado;
     List<Paint> mPaintList;
     List<Path> mPathList;
     Paint currentPaint;
     Path currentPath;
     ColorDrawable currentColor;
 
-    public SimplePaint(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+    float startX, startY;
+
+    public void setup(){
         mPaintList=new ArrayList<Paint>();
         mPathList=new ArrayList<Path>();
         currentColor=new ColorDrawable();
         currentColor.setColor(Color.BLACK);
         initlayerDraw();
     }
+
+    public SimplePaint(Context context) {
+        super(context);
+        setup();
+    }
+
+    public SimplePaint(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        setup();
+
+    }
+
+    public SimplePaint(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setup();
+
+    }
+
+    public SimplePaint(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setup();
+
+    }
+
     public void initlayerDraw(){
         //configurando paint
         currentPaint=new Paint();
@@ -47,6 +73,7 @@ public class SimplePaint extends View {
         for (int i=0; i<mPathList.size();i++){
             canvas.drawPath(mPathList.get(i), mPaintList.get(i));
         }
+        canvas.drawPath(currentPath, currentPaint);
     }
 
     @Override
@@ -55,29 +82,107 @@ public class SimplePaint extends View {
         lx = event.getX();
         ly = event.getY();
 
-        switch (event.getAction()) {
-            case (MotionEvent.ACTION_DOWN):
-                currentPath.moveTo(lx, ly);
-                currentPath.lineTo(lx, ly);
+        switch (botaoSelecionado) {
+
+            case 1:
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        currentPath.moveTo(lx, ly);
+                        startX = lx;
+                        startY = ly;
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+                        currentPath.reset();
+                        currentPath.moveTo(startX, startY);
+                        currentPath.lineTo(lx, ly);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        mPaintList.add(currentPaint);
+                        mPathList.add(currentPath);
+                        initLayerDraw();
+                        break;
+                }
                 break;
-            case (MotionEvent.ACTION_MOVE):
-                currentPath.lineTo(lx, ly);
+
+            case 2:
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        startX = lx;
+                        startY = ly;
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+                        float radiusMove = (float) Math.sqrt(
+                                Math.pow(lx - startX, 2) +
+                                        Math.pow(ly - startY, 2)
+                        );
+                        currentPath.reset();
+                        currentPath.addCircle(startX, startY, radiusMove, Path.Direction.CW);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        float radiusEnd = (float) Math.sqrt(
+                                Math.pow(lx - startX, 2) +
+                                        Math.pow(ly - startY, 2)
+                        );
+                        currentPath.reset();
+                        currentPath.addCircle(startX, startY, radiusEnd, Path.Direction.CW);
+
+                        mPaintList.add(currentPaint);
+                        mPathList.add(currentPath);
+                        initLayerDraw();
+                        break;
+                }
                 break;
-            case (MotionEvent.ACTION_UP):
-                currentPath.lineTo(lx, ly);
-                mPaintList.add(currentPaint);
-                mPathList.add(currentPath);
-                currentPaint=new Paint();
-                currentPath=new Path();
-                initlayerDraw();
-                break;
-            default:
+
+            case 3:
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        startX = lx;
+                        startY = ly;
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+                        float left = Math.min(startX, lx);
+                        float top = Math.min(startY, ly);
+                        float right = Math.max(startX, lx);
+                        float bottom = Math.max(startY, ly);
+
+                        currentPath.reset();
+                        currentPath.addRect(left, top, right, bottom, Path.Direction.CW);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        float L = Math.min(startX, lx);
+                        float T = Math.min(startY, ly);
+                        float R = Math.max(startX, lx);
+                        float B = Math.max(startY, ly);
+
+                        currentPath.reset();
+                        currentPath.addRect(L, T, R, B, Path.Direction.CW);
+
+                        mPaintList.add(currentPaint);
+                        mPathList.add(currentPath);
+                        initLayerDraw();
+                        break;
+                }
                 break;
         }
         invalidate();
         return true;
     }
 
+    private void initLayerDraw() {
+    }
+
+    public void limparPaint(){
+        mPaintList.clear();
+        mPathList.clear();
+        currentPath.reset();
+        invalidate();
+    }
     public void setColor(int color) {
         currentColor.setColor(color);
         currentPaint.setColor(color);
