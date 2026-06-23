2c72328a037b9fec4507a13ccd2df66ec0463208
Natasha <natashamariadealmeida@gmail.com>
2026-06-22 21:51:06 -0300

    PaintExemploAula

---
+++ app/src/main/java/com/natasha/mypaint/SimplePaint.java
@@ -0,0 +1,57 @@
+package com.natasha.mypaint;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class SimplePaint extends View {
+    Paint mPaint;
+    Path mPath;
+    public SimplePaint(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        mPaint=new Paint();
+        mPath=new Path();
+        //configurando paint
+        mPaint.setColor(Color.RED);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(20);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float ly, lx;
+        lx = event.getX();
+        ly = event.getY();
+
+        switch (event.getAction()) {
+            case (MotionEvent.ACTION_DOWN):
+                mPath.moveTo(lx, ly);
+                mPath.lineTo(lx, ly);
+                break;
+            case (MotionEvent.ACTION_MOVE):
+                mPath.lineTo(lx, ly);
+                break;
+            case (MotionEvent.ACTION_UP):
+                mPath.lineTo(lx, ly);
+                break;
+            default:
+                break;
+        }
+        invalidate();
+        return true;
+    }
+}
+
