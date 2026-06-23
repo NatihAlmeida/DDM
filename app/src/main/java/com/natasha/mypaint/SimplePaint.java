c9d5dadbe5d64d32e98a85c98596b0585f23e695
Natasha <natashamariadealmeida@gmail.com>
2026-06-22 23:07:23 -0300

    SimplePaint

--- app/src/main/java/com/natasha/mypaint/SimplePaint.java
+++ app/src/main/java/com/natasha/mypaint/SimplePaint.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.drawable.ColorDrawable;
 import android.hardware.lights.LightState;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -12,6 +13,7 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SimplePaint extends View {
@@ -19,22 +21,32 @@ public class SimplePaint extends View {
     List<Path> mPathList;
     Paint currentPaint;
     Path currentPath;
+    ColorDrawable currentColor;
 
     public SimplePaint(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-
+        mPaintList=new ArrayList<Paint>();
+        mPathList=new ArrayList<Path>();
+        currentColor=new ColorDrawable();
+        currentColor.setColor(Color.BLACK);
+        initlayerDraw();
+    }
+    public void initlayerDraw(){
+        //configurando paint
         currentPaint=new Paint();
         currentPath=new Path();
-        //configurando paint
-        currentPaint.setColor(Color.RED);
         currentPaint.setStyle(Paint.Style.STROKE);
         currentPaint.setStrokeWidth(20);
-    }
+        currentPaint.setColor(currentColor.getColor());
 
+    }
     @Override
     protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
         canvas.drawPath(currentPath, currentPaint);
+        for (int i=0; i<mPathList.size();i++){
+            canvas.drawPath(mPathList.get(i), mPaintList.get(i));
+        }
     }
 
     @Override
@@ -53,6 +65,11 @@ public class SimplePaint extends View {
                 break;
             case (MotionEvent.ACTION_UP):
                 currentPath.lineTo(lx, ly);
+                mPaintList.add(currentPaint);
+                mPathList.add(currentPath);
+                currentPaint=new Paint();
+                currentPath=new Path();
+                initlayerDraw();
                 break;
             default:
                 break;
@@ -62,6 +79,7 @@ public class SimplePaint extends View {
     }
 
     public void setColor(int color) {
+        currentColor.setColor(color);
         currentPaint.setColor(color);
     }
 }
